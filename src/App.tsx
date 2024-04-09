b384d21374ca5c7722fb374c1a2a672a633e235c
gazzati <gazzaevtimur@gmail.com>
2024-04-09 20:34:59 +0300

    add actions

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,48 @@
-import { Logo } from "@components/Icons/Logo"
+import { Logo, Mic, MicOff, Dynamic, DynamicOff, VideoOn, VideoOff } from "@components/Icons"
 import RemoteVideo from "@components/RemoteVideo"
 import Video from "@components/Video"
-import React, { useRef } from "react"
+import React, { useState, useRef } from "react"
 
 import { useCreateMediaStream } from "./hooks/useCreateMediaStream"
 import { useStartPeerSession } from "./hooks/useStartPeerSession"
 import styles from "./style.m.scss"
 
 const App = () => {
+  const [muted, setMuted] = useState(false)
+  const [dynamicOff, setDynamicState] = useState(false)
+  const [videoOff, setVideoState] = useState(false)
+
   const localVideoRef = useRef<HTMLVideoElement>(null)
   const userMediaStream = useCreateMediaStream(localVideoRef)
 
   const { destination } = useStartPeerSession(userMediaStream)
+
+  const handleMute = () => {
+    userMediaStream?.getTracks()?.forEach(track => {
+      if (track?.kind === "audio") {
+        track.enabled = muted
+        setMuted(!muted)
+      }
+    })
+  }
+
+  const handleVideoToggle = () => {
+    userMediaStream?.getTracks()?.forEach(track => {
+      if (track?.kind === "video") {
+        track.enabled = videoOff
+        setVideoState(!videoOff)
+      }
+    })
+  }
+
+  const handleDynamicToggle = () => {
+    if(!destination) return
+
+    const stream = (document.getElementById(destination) as HTMLVideoElement)
+    stream.volume = dynamicOff ? 1 : 0
+
+    setDynamicState(!dynamicOff)
+  }
 
   return (
     <div className={styles.talker}>
@@ -20,8 +51,28 @@ const App = () => {
       </div>
 
       <div className={styles.videosContainer}>
-        <Video id="local" className={styles.localVideo} ref={localVideoRef} muted={true} />
+        <Video
+          id="local"
+          className={destination ? styles.localVideo : styles.remoteVideo}
+          ref={localVideoRef}
+          muted={true}
+        />
         {destination && <RemoteVideo id={destination} />}
+      </div>
+
+      <div className={styles.actions}>
+        <div className={styles.action} onClick={handleMute}>
+          {muted ? <MicOff /> : <Mic />}
+        </div>
+
+        <div className={styles.action} onClick={handleVideoToggle}>
+          {videoOff ? <VideoOff /> : <VideoOn />}
+        </div>
+
+        {!destination &&  <div className={styles.action} onClick={handleDynamicToggle}>
+          {dynamicOff ? <DynamicOff /> : <Dynamic />}
+        </div>}
+
       </div>
     </div>
   )
---
+++ src/components/Icons/index.ts
@@ -0,0 +1,9 @@
+import { Dynamic } from "./Dynamic"
+import { DynamicOff } from "./DynamicOff"
+import { Logo } from "./Logo"
+import { Mic } from "./Mic"
+import { MicOff } from "./MicOff"
+import { VideoOff } from "./VideoOff"
+import { VideoOn } from "./VideoOn"
+
+export { Logo, Mic, MicOff, Dynamic, DynamicOff, VideoOn, VideoOff }
